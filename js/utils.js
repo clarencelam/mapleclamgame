da641bbd1a959c3313fa9eaf3ba2535eaa17dc4f
clarencelam <lam.clarence.bc@gmail.com>
2023-06-30 17:20:33 -0700

    updates

---
+++ js/utils.js
@@ -0,0 +1,10 @@
+function startRolls(sprite, decisionInterval, max){
+    // generate 
+    // may need to revisit this later to ensure Timeout is deleted when object is removed
+    function roll(){
+        setTimeout(roll,decisionInterval)
+        let num = Math.floor(Math.random()* max)
+        sprite.movementDecision(num)
+    }
+    roll()
+}
